cceef38ed1eacbd4777700c47633f91ca8a6e48f
Jarek <jarek@jdomanski79.nazwa.pl>
2018-12-06 15:09:02 +0100

    implement thymeleaf layout dialect

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,6 @@ values
 (0,false,false,' Komputer HP Compaq ',' 2388/287 ');
 
 insert into user (username,  password, CREDENTIALS_NON_EXPIRED, name)
-values ('jarek','$2a$04$Q1a7bH9YPQ9S3YlZdPGudejvJRrUmGwph2qBdGoF66aefU5OaU40G', true, 'JD');
+values 
+('jarek','$2a$04$Q1a7bH9YPQ9S3YlZdPGudejvJRrUmGwph2qBdGoF66aefU5OaU40G', true, 'JD'),
+('mariusz', '$2a$12$BEpUZhok00A8oUL.XT4X2ezZ9A01eFKEZxD9qyWv2Plm9X8H8RE/K', true, 'MZ');
