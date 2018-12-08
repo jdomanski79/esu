3f3112d262760d36388e9768b7b11e802c0042c8
Jarek <jarek@jdomanski79.nazwa.pl>
2018-12-08 23:50:41 +0100

    add countByState method

--- src/main/java/pl/jdomanski/esu/equipment/EquipmentRepository.java
+++ src/main/java/pl/jdomanski/esu/equipment/EquipmentRepository.java
@@ -2,7 +2,9 @@ package pl.jdomanski.esu.equipment;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.jdomanski.esu.equipmentEvent.EquipmentState;
 
 @Repository
 public interface EquipmentRepository extends CrudRepository<Equipment, Long> {
+    Long countByState(EquipmentState state);
 }
